f9d437f8a8ca37ba56922f7060d7b89a489f6441
George Song <george@55minutes.com>
2015-08-17 17:02:45 -0700

    Use gulp-load-plugins

--- tasks/build.js
+++ tasks/build.js
@@ -1,23 +1,20 @@
 import * as compilerOptions from './settings/babel';
 import * as paths from './settings/paths';
-import babel from 'gulp-babel';
-import changed from 'gulp-changed';
 import gulp from 'gulp';
-import plumber from 'gulp-plumber';
+import gulpLoadPlugins from 'gulp-load-plugins';
 import runSequence from 'run-sequence';
-import sass from 'gulp-sass';
-import sourcemaps from 'gulp-sourcemaps';
 
 
+const $ = gulpLoadPlugins();
 let assign = Object.assign
 
 gulp.task('build-system', () =>
   gulp.src(paths.source)
-  .pipe(plumber())
-  .pipe(changed(paths.output, {extension: '.js'}))
-  .pipe(sourcemaps.init({loadMaps: true}))
-  .pipe(babel(assign({}, compilerOptions, {modules:'system'})))
-  .pipe(sourcemaps.write({
+  .pipe($.plumber())
+  .pipe($.changed(paths.output, {extension: '.js'}))
+  .pipe($.sourcemaps.init({loadMaps: true}))
+  .pipe($.babel(assign({}, compilerOptions, {modules:'system'})))
+  .pipe($.sourcemaps.write({
     includeContent: false, sourceRoot: paths.sourceMapRelativePath
   }))
   .pipe(gulp.dest(paths.output))
@@ -25,17 +22,17 @@ gulp.task('build-system', () =>
 
 gulp.task('build-html', () =>
   gulp.src(paths.html)
-  .pipe(changed(paths.output, {extension: '.html'}))
+  .pipe($.changed(paths.output, {extension: '.html'}))
   .pipe(gulp.dest(paths.output))
 );
 
 gulp.task('build-styles', () =>
   gulp.src(paths.style)
-  .pipe(plumber())
-  .pipe(changed(paths.output, {extension: '.css'}))
-  .pipe(sourcemaps.init({loadMaps: true}))
-  .pipe(sass().on('error', sass.logError))
-  .pipe(sourcemaps.write({
+  .pipe($.plumber())
+  .pipe($.changed(paths.output, {extension: '.css'}))
+  .pipe($.sourcemaps.init({loadMaps: true}))
+  .pipe($.sass().on('error', $.sass.logError))
+  .pipe($.sourcemaps.write({
     includeContent: false, sourceRoot: paths.sourceMapRelativePath
   }))
   .pipe(gulp.dest(paths.output))

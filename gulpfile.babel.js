b238173cfb0ced0545574b4511b6a830546db78b
George Song <george@55minutes.com>
2015-08-17 18:21:42 -0700

    Refactor gulp tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,8 @@
 /*eslint-disable no-console */
+import gulp from 'gulp';
 import requireDir from 'require-dir';
 
 // Load custom tasks from the `tasks` directory
 try { requireDir('tasks'); } catch (err) { console.error(err); }
+
+gulp.task('default', ['serve:dev'], () => {});
--- tasks/serve.js
+++ tasks/serve.js
@@ -1,22 +1,43 @@
+/*eslint-disable no-console */
 import browserSync from 'browser-sync';
 import gulp from 'gulp';
 
-import {srcDir, buildDir} from './settings/paths';
+import * as paths from './settings/paths';
 
+
+const reload = browserSync.reload;
+
+function reportChange(event) {
+  console.log(
+    `File ${event.path} was ${event.type}, running tasks…`
+  );
+}
 
 // this task utilizes the browsersync plugin
 // to create a dev server instance
 // at http://localhost:9000
 gulp.task('serve', ['build'], (done) => {
   browserSync({
+    ghostMode: false,
     open: false,
+    notify: false,
     port: 9000,
     server: {
-      baseDir: ['.', srcDir, buildDir],
+      baseDir: ['.', paths.srcDir, paths.buildDir],
       middleware: (req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
         next();
       }
     }
   }, done);
+});
+
+gulp.task('serve:dev', ['serve'], () => {
+  gulp.watch(paths.scriptSrc, ['build-system', reload]).on(
+    'change', reportChange
+  );
+  gulp.watch(paths.htmlSrc, ['build-html', reload]).on('change', reportChange);
+  gulp.watch(paths.styleSrc, ['build-styles', reload]).on(
+    'change', reportChange
+  );
 });

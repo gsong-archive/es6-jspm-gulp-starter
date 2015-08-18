a56ef1b0650ad658ea6147a87e64e386041d8fa9
George Song <george@55minutes.com>
2015-08-17 17:13:13 -0700

    Minor refactor

--- tasks/watch.js
+++ tasks/watch.js
@@ -5,6 +5,8 @@ import browserSync from 'browser-sync';
 import gulp from 'gulp';
 
 
+const reload = browserSync.reload;
+
 function reportChange(event) {
   console.log(
     `File ${event.path} was ${event.type}, running tasks…`
@@ -12,13 +14,7 @@ function reportChange(event) {
 }
 
 gulp.task('watch', ['serve'], () => {
-  gulp.watch(paths.source, ['build-system', browserSync.reload]).on(
-    'change', reportChange
-  );
-  gulp.watch(paths.html, ['build-html', browserSync.reload]).on(
-    'change', reportChange
-  );
-  gulp.watch(paths.style, ['build-styles', browserSync.reload]).on(
-    'change', reportChange
-  );
+  gulp.watch(paths.source, ['build-system', reload]).on('change', reportChange);
+  gulp.watch(paths.html, ['build-html', reload]).on('change', reportChange);
+  gulp.watch(paths.style, ['build-styles', reload]).on('change', reportChange);
 });

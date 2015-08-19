8d42bc71639f8bd7d5998e09d29313e01c377e6d
George Song <george@55minutes.com>
2015-08-19 08:26:23 -0700

    Add autoprefixer support

--- tasks/style.js
+++ tasks/style.js
@@ -8,8 +8,20 @@ import * as paths from './paths';
 const $ = gulpLoadPlugins();
 
 
-gulp.task('compile:styles', () =>
-  gulp.src(paths.SRC_STYLE)
+gulp.task('compile:styles', () => {
+  const AUTOPREFIXER_BROWSERS = [
+    'ie >= 10',
+    'ie_mob >= 10',
+    'ff >= 30',
+    'chrome >= 34',
+    'safari >= 7',
+    'opera >= 23',
+    'ios >= 7',
+    'android >= 4.4',
+    'bb >= 10'
+  ];
+
+  return gulp.src(paths.SRC_STYLE)
   .pipe($.plumber({
     errorHandler: (err) => {
       console.log(err);
@@ -18,5 +30,6 @@ gulp.task('compile:styles', () =>
   }))
   .pipe($.changed(paths.BUILD_DIR, {extension: '.css'}))
   .pipe($.sass().on('error', $.sass.logError))
+  .pipe($.autoprefixer(AUTOPREFIXER_BROWSERS))
   .pipe(gulp.dest(paths.TMP_DIR))
-);
+});

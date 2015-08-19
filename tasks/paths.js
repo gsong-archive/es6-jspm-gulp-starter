26e82d1b68a9b1e4085e0587cc25838d5e309975
George Song <george@55minutes.com>
2015-08-18 22:21:20 -0700

    Clean up paths

--- tasks/paths.js
+++ tasks/paths.js
@@ -3,9 +3,9 @@ import path from 'path';
 
 export let buildDir = '.build/';
 export let distDir = 'dist/';
-export let jspmDir = 'jspm_packages';
 export let srcDir = 'app/';
 export let tmpDir = '.tmp/';
+let jspmDir = 'jspm_packages';
 
 export let buildAll = path.join(buildDir, '**/*');
 export let buildScriptsDir = path.join(buildDir, 'scripts/');
@@ -17,9 +17,6 @@ export let srcStyle = path.join(srcDir, '**/*.scss');
 
 export let tmpImage = path.join(tmpDir, '**/*.+(png|jpg|svg)');
 export let tmpScriptsDir = path.join(tmpDir, 'scripts/');
-export let tmpStyle = path.join(tmpDir, '**/*.scss');
 
 export let fontAwesomePath = path.join(jspmDir, 'npm/font-awesome@4.4.0/');
 export let fontSrc = path.join(fontAwesomePath, 'fonts/*');
-
-export let sourceMapRelativePath = path.join('../', srcDir);

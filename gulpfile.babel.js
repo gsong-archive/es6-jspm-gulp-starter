ac9abd00785f4d33a2938fb6910797c68b168d30
George Song <george@55minutes.com>
2015-10-02 14:30:19 -0700

    Use gulp-util for logging instead of console

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,14 @@
-/*eslint-disable no-console */
 import gulp from 'gulp';
+import gulpUtil from 'gulp-util';
 import requireDir from 'require-dir';
 
+
 // Load custom tasks from the `tasks` directory
-try { requireDir('tasks'); } catch (err) { console.error(err); }
+try {
+  requireDir('tasks');
+} catch (err) {
+  gulpUtil.log(err);
+}
+
 
 gulp.task('default', ['serve:dev'], () => {});

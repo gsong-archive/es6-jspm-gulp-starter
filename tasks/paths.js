9a33df9e3510d22e4bc55374740f6cfb29c28272
George Song <george@55minutes.com>
2015-10-02 14:30:47 -0700

    Improve path naming

--- tasks/paths.js
+++ tasks/paths.js
@@ -10,7 +10,7 @@ export const BUILD_ALL = path.join(BUILD_DIR, '**/*');
 export const BUILD_SCRIPTS_DIR = path.join(BUILD_DIR, 'scripts/');
 
 export const SRC_ALL = path.join(SRC_DIR, '**/*');
-export const SRC_HTML = path.join(SRC_DIR, 'index.html');
+export const SRC_INDEX = path.join(SRC_DIR, 'index.html');
 export const SRC_SCRIPT = path.join(SRC_DIR, '**/*.js');
 export const SRC_STYLE = path.join(SRC_DIR, '**/*.scss');
 
@@ -19,3 +19,7 @@ export const TMP_SCRIPTS_DIR = path.join(TMP_DIR, 'scripts/');
 
 export const FONT_AWESOME_PATH = 'jspm_packages/npm/font-awesome@4.4.0/';
 export const FONT_SRC = path.join(FONT_AWESOME_PATH, 'fonts/*');
+
+const SCRIPT = 'main';
+export const MAIN_SRC = path.join(TMP_SCRIPTS_DIR, SCRIPT);
+export const MAIN_DEST = path.join(BUILD_SCRIPTS_DIR, SCRIPT + '.js');
